cc7c36c9b8a513b3fae1ee559400b9df62048ddb
tommy <651775527@qq.com>
2017-07-31 11:06:55 +0800

    .

---
+++ resources/views/company/cmsa/bottom.blade.php
@@ -0,0 +1,21 @@
+
+<table width="958" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td align="center">&nbsp;</td>
+  </tr>
+  <tr>
+  <td align="center">版权所有Copyright &copy; 2008-2018  中国民生智库信息中心  京ICP备09033294号-4 </td>
+  </tr>
+  <tr>
+    <td align="center"> 邮箱：cmsa@cmsa.org.cn 地址：北京市海淀区万寿路甲12号D座7层 </td>
+  </tr>
+  <tr>
+    <td align="center"><script src="http://s19.cnzz.com/stat.php?id=4888214&web_id=4888214&show=pic" language="JavaScript"></script></td>
+  </tr>
+  <tr>
+    <td align="center">&nbsp;</td>
+  </tr>
+</table>
+
+</body>
+</html>
